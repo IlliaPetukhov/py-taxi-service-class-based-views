a8e9446c0242037dd306e6a3468a3a1dfe320492
Illia <ilaruslanovich7@gmail.com>
2024-05-25 18:47:52 +0200

    Sol3

--- taxi/urls.py
+++ taxi/urls.py
@@ -9,12 +9,12 @@ from .views import (index,
 
 urlpatterns = [
     path("", index, name="index"),
-    path("manufacturer/", ManufacturerListView.as_view(), name="manufacturer"),
-    path("cars/", CarListView.as_view(), name="cars"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car_detail"),
-    path("drivers/", DriverListView.as_view(), name="drivers"),
+    path("manufacturers/", ManufacturerListView.as_view(), name="manufacturer-list"),
+    path("cars/", CarListView.as_view(), name="car-list"),
+    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
+    path("drivers/", DriverListView.as_view(), name="driver-list"),
     path("drivers/<int:pk>/", DriverDetailView.as_view(),
-         name="driver_detail"),
+         name="driver-detail"),
 ]
 
 app_name = "taxi"
